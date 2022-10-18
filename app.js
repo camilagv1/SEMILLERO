7161e7e250ada1a9807a6ab01ca44959f3a5f1e9
camilagv1 <mcgrajales071@misena.edu.co>
2022-10-18 10:59:58 -0500

    avances

---
+++ app.js
@@ -0,0 +1,4 @@
+console.log('hello world')
+
+import {data} from "./data/data.js"
+console.log(data);
---
+++ data/data.js
@@ -0,0 +1,22 @@
+//Constante en donde estara la info 
+export const data =[
+
+    {
+        question:"Cual de las siguientes opciones no es un editor de codigo?",
+        choices:["vim", "vscode", "atom","word"],
+        answuer:"word",
+    
+    },
+    {
+        question:"Que lenguaje no es orientado a objetos?",
+        choices:["java", "haskell", "c++","python"],
+        answuer:"haskell",
+    
+    },
+    {
+        question:"Que lenguaje no sirve para estilizar sitios web?",
+        choices:["stylus", "less", "sass","PostCSS","NextCss"],
+        answuer:"NextCss",
+    
+    },
+];
